77dbaa9da62e28fb2c3440f1f865ee4c60b51d3b
pipedrimer <pipedrimer@gmail.com>
2025-05-01 08:30:12 -0700

    roadmap

--- src/component/Roadmap.tsx
+++ src/component/Roadmap.tsx
@@ -179,7 +179,7 @@ export function Roadmap() {
   }
 
   return (
-    <div className="space-y-8 w-2/3 justify-center align-middle">
+    <div className="space-y-8 w-2/3 ">
      
       <div className="relative">
         <div className="absolute left-0 right-0 h-1 top-4 bg-gray-800 rounded-full">
