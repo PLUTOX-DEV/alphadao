2a339715789ebe50d0acb81115ffc08de3609ca8
pipedrimer <pipedrimer@gmail.com>
2025-05-06 14:49:39 -0700

    hero

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion";
-
+import {useState} from "react";
+import { Link } from "react-router-dom";
 export default function Hero() {
+
+  const [active, setActive] = useState(false);
   return (
     <div className="relative z-10 min-h-130 flex items-center">
       <div className="container mx-auto px-6">
@@ -17,11 +20,13 @@ export default function Hero() {
                   ALPHA DAO
                 </span>
               </div>
-              <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mt-4 me-2 overflow-hidden text-sm font-medium text-white-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white  focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
-                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+              <Link onClick={() => setActive(!active)} to="/">
+              <button  className="relative inline-flex items-center justify-center p-0.5 mb-2 mt-4 me-2 overflow-hidden text-sm font-medium text-white-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white  focus:bg-gray-950 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent active:bg-pink-400">
                   Get Started
                 </span>
               </button>
+              </Link>
             </h1>
           </motion.div>
         </div>
