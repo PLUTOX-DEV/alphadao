8674fb6ecab2ec5ebfff5bb2ec8c585c13dfe2a2
Plutox <151991623+PLUTOX-DEV@users.noreply.github.com>
2025-07-23 04:06:54 -0700

    Update server.js

--- alpha-dao-backend/src/server.js
+++ alpha-dao-backend/src/server.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://alphadao.vercel.app",
     credentials: true,
   })
 );
